49daae63fe3a5b25b4b61333782648c4fbc4b8ff
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2024-05-17 14:20:23 +0200

    ✅1.2

---
+++ part1/courseInfo/src/App.jsx
@@ -0,0 +1,45 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ data }) => {
+  return (
+    <p>
+      {data.part} {data.exercises}
+    </p>
+  );
+};
+
+const Content = ({ data }) => {
+  return data.map((item, index) => <Part key={index} data={item} />);
+};
+
+const Total = ({ number }) => {
+  return <p>Number of exercises {number}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        data={[
+          { part: part1, exercises: exercises1 },
+          { part: part2, exercises: exercises2 },
+          { part: part3, exercises: exercises3 },
+        ]}
+      />
+      <Total number={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+export default App;
