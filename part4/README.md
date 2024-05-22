61097bb4c9fb3c7bd54005cf2333b56dcc3f769e
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2024-05-22 14:42:09 +0200

    ✅ 4.2

---
+++ part4/README.md
@@ -0,0 +1,1 @@
+## [Hello](https://morenno.net/)
---
+++ part4/app.js
@@ -0,0 +1,48 @@
+const config = require("./utils/config");
+
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const cors = require("cors");
+const blogRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+
+app.use(middleware.requestLogger);
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :content"
+  )
+);
+
+morgan.token("content", (request) =>
+  request.method === "POST" && request.body.name
+    ? JSON.stringify(request.body)
+    : null
+);
+
+app.use("/api/blogs", blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
