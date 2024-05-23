86eb7c112f8cf9f7c3fad858ffbebb83f4688186
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2024-05-23 12:58:13 +0200

    ✅ 4.14

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -18,4 +18,24 @@ blogsRouter.post("/", async (request, response) => {
   response.status(201).json(result);
 });
 
+blogsRouter.delete("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+
+  if (!blog) return response.status(404).end();
+
+  await Blog.findByIdAndDelete(request.params.id);
+
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const blog = request.body;
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+
+  response.json(updatedBlog);
+});
+
 module.exports = blogsRouter;
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
 const assert = require("node:assert");
 const supertest = require("supertest");
 const mongoose = require("mongoose");
@@ -8,103 +8,165 @@ const api = supertest(app);
 
 const Blog = require("../models/blog");
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
+describe("when there is initially some blogs saved", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
 
-  let blogObject = new Blog(helper.initialBlogs[0]);
-  await blogObject.save();
+    let blogObject = new Blog(helper.initialBlogs[0]);
+    await blogObject.save();
 
-  blogObject = new Blog(helper.initialBlogs[1]);
-  await blogObject.save();
+    blogObject = new Blog(helper.initialBlogs[1]);
+    await blogObject.save();
+  });
+
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("there are two blogs", async () => {
+    const response = await api.get("/api/blogs");
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
+  });
+
+  test("ID written as so and not _id", async () => {
+    const response = await api.get("/api/blogs");
+
+    assert(Object.keys(response.body[0]).includes("id"));
+    assert(!Object.keys(response.body[0]).includes("_id"));
+  });
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("addition of a new blog", () => {
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map((r) => r.title);
+    assert(titles.includes("Canonical string reduction"));
+  });
+
+  test("if LIKES property is missing, it will default to 0", async () => {
+    const newBlog = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    const addedBlog = blogsAtEnd.find(
+      (blog) => blog.title === "Canonical string reduction"
+    );
+    assert.strictEqual(addedBlog.likes, 0);
+  });
+
+  test("if TITLE property is missing, return 400", async () => {
+    const newBlog = {
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
+
+  test("if URL property is missing, return 400", async () => {
+    const newBlog = {
+      author: "Edsger W. Dijkstra",
+      title: "Canonical string reduction",
+      likes: 12,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
 });
 
-test("there are two blogs", async () => {
-  const response = await api.get("/api/blogs");
+describe("deletion of a blog", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
 
-  assert.strictEqual(response.body.length, helper.initialBlogs.length);
+    let blogObject = new Blog(helper.initialBlogs[0]);
+    await blogObject.save();
+
+    blogObject = new Blog(helper.initialBlogs[1]);
+    await blogObject.save();
+  });
+
+  test("a blog can be deleted", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+
+    const titles = blogsAtEnd.map((r) => r.title);
+
+    assert(!titles.includes(blogToDelete.title));
+  });
 });
 
-test("ID written as so and not _id", async () => {
-  const response = await api.get("/api/blogs");
+describe.only("updating a blog", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
 
-  assert(Object.keys(response.body[0]).includes("id"));
-  assert(!Object.keys(response.body[0]).includes("_id"));
-});
+    let blogObject = new Blog(helper.initialBlogs[0]);
+    await blogObject.save();
 
-test("a valid blog can be added", async () => {
-  const newBlog = {
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-  };
+    blogObject = new Blog(helper.initialBlogs[1]);
+    await blogObject.save();
+  });
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+  test.only("blog likes can be updated", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
 
-  const blogsAtEnd = await helper.blogsInDb();
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+    const updatedBlog = { ...blogToUpdate, likes: blogToUpdate.likes + 1 };
 
-  const titles = blogsAtEnd.map((r) => r.title);
-  assert(titles.includes("Canonical string reduction"));
-});
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200);
 
-test("if LIKES property is missing, it will default to 0", async () => {
-  const newBlog = {
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-  };
+    const blogsAtEnd = await helper.blogsInDb();
+    const updatedBlogAtEnd = blogsAtEnd.find(
+      (blog) => blog.id === blogToUpdate.id
+    );
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  const addedBlog = blogsAtEnd.find(
-    (blog) => blog.title === "Canonical string reduction"
-  );
-  assert.strictEqual(addedBlog.likes, 0);
-});
-
-test.only("if TITLE property is missing, return 400", async () => {
-  const newBlog = {
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-  };
-
-  await api.post("/api/blogs").send(newBlog).expect(400);
-
-  const blogsAtEnd = await helper.blogsInDb();
-
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
-});
-
-test.only("if URL property is missing, return 400", async () => {
-  const newBlog = {
-    author: "Edsger W. Dijkstra",
-    title: "Canonical string reduction",
-    likes: 12,
-  };
-
-  await api.post("/api/blogs").send(newBlog).expect(400);
-
-  const blogsAtEnd = await helper.blogsInDb();
-
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+    assert.strictEqual(updatedBlogAtEnd.likes, blogToUpdate.likes + 1);
+  });
 });
 
 after(async () => {
