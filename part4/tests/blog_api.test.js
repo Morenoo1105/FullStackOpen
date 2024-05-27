c2ea051d99ab60052afeaef3782ebc6ac6b65bd7
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2024-05-27 13:10:15 +0200

    ✅ 4.23

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -2,23 +2,47 @@ const { test, after, beforeEach, describe } = require("node:test");
 const assert = require("node:assert");
 const supertest = require("supertest");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const helper = require("./test_helper");
 const app = require("../app");
 const api = supertest(app);
 
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
-describe("when there is initially some blogs saved", () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({});
+beforeEach(async () => {
+  await User.deleteMany({});
 
-    let blogObject = new Blog(helper.initialBlogs[0]);
-    await blogObject.save();
-
-    blogObject = new Blog(helper.initialBlogs[1]);
-    await blogObject.save();
+  const passwordHash = await bcrypt.hash("password", 10);
+  const user = new User({
+    username: "superuser",
+    name: "Superuser",
+    blogs: [],
+    passwordHash,
   });
 
+  await user.save();
+});
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const users = await User.find({});
+  const user = users[0];
+  const id = user._id;
+
+  let blogObject = new Blog({ ...helper.initialBlogs[0], user: id });
+  await blogObject.save();
+  user.blogs = user.blogs.concat(blogObject._id);
+
+  blogObject = new Blog({ ...helper.initialBlogs[1], user: id });
+  await blogObject.save();
+  user.blogs = user.blogs.concat(blogObject._id);
+
+  await user.save();
+});
+
+describe("when there is initially some blogs saved", () => {
   test("blogs are returned as json", async () => {
     await api
       .get("/api/blogs")
@@ -41,6 +65,18 @@ describe("when there is initially some blogs saved", () => {
 });
 
 describe("addition of a new blog", () => {
+  let headers;
+
+  beforeEach(async () => {
+    const response = await api
+      .post("/api/login")
+      .send({ username: "superuser", password: "password" });
+
+    headers = {
+      Authorization: `Bearer ${response.body.token}`,
+    };
+  });
+
   test("a valid blog can be added", async () => {
     const newBlog = {
       title: "Canonical string reduction",
@@ -53,6 +89,7 @@ describe("addition of a new blog", () => {
       .post("/api/blogs")
       .send(newBlog)
       .expect(201)
+      .set(headers)
       .expect("Content-Type", /application\/json/);
 
     const blogsAtEnd = await helper.blogsInDb();
@@ -60,6 +97,17 @@ describe("addition of a new blog", () => {
 
     const titles = blogsAtEnd.map((r) => r.title);
     assert(titles.includes("Canonical string reduction"));
+  });
+
+  test("adding blog without jwt token", async () => {
+    const newBlog = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(401);
   });
 
   test("if LIKES property is missing, it will default to 0", async () => {
@@ -73,6 +121,7 @@ describe("addition of a new blog", () => {
       .post("/api/blogs")
       .send(newBlog)
       .expect(201)
+      .set(headers)
       .expect("Content-Type", /application\/json/);
 
     const blogsAtEnd = await helper.blogsInDb();
@@ -89,7 +138,7 @@ describe("addition of a new blog", () => {
       likes: 12,
     };
 
-    await api.post("/api/blogs").send(newBlog).expect(400);
+    await api.post("/api/blogs").send(newBlog).set(headers).expect(400);
 
     const blogsAtEnd = await helper.blogsInDb();
 
@@ -103,7 +152,7 @@ describe("addition of a new blog", () => {
       likes: 12,
     };
 
-    await api.post("/api/blogs").send(newBlog).expect(400);
+    await api.post("/api/blogs").send(newBlog).set(headers).expect(400);
 
     const blogsAtEnd = await helper.blogsInDb();
 
@@ -112,21 +161,23 @@ describe("addition of a new blog", () => {
 });
 
 describe("deletion of a blog", () => {
+  let headers;
+
   beforeEach(async () => {
-    await Blog.deleteMany({});
+    const response = await api
+      .post("/api/login")
+      .send({ username: "superuser", password: "password" });
 
-    let blogObject = new Blog(helper.initialBlogs[0]);
-    await blogObject.save();
-
-    blogObject = new Blog(helper.initialBlogs[1]);
-    await blogObject.save();
+    headers = {
+      Authorization: `Bearer ${response.body.token}`,
+    };
   });
 
   test("a blog can be deleted", async () => {
     const blogsAtStart = await helper.blogsInDb();
     const blogToDelete = blogsAtStart[0];
 
-    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204).set(headers);
 
     const blogsAtEnd = await helper.blogsInDb();
 
@@ -136,17 +187,30 @@ describe("deletion of a blog", () => {
 
     assert(!titles.includes(blogToDelete.title));
   });
+
+  test("a blog CANNOT be deleted without jwt", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(401);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
 });
 
 describe("updating a blog", () => {
+  let headers;
+
   beforeEach(async () => {
-    await Blog.deleteMany({});
+    const response = await api
+      .post("/api/login")
+      .send({ username: "superuser", password: "password" });
 
-    let blogObject = new Blog(helper.initialBlogs[0]);
-    await blogObject.save();
-
-    blogObject = new Blog(helper.initialBlogs[1]);
-    await blogObject.save();
+    headers = {
+      Authorization: `Bearer ${response.body.token}`,
+    };
   });
 
   test("blog likes can be updated", async () => {
@@ -158,7 +222,8 @@ describe("updating a blog", () => {
     await api
       .put(`/api/blogs/${blogToUpdate.id}`)
       .send(updatedBlog)
-      .expect(200);
+      .expect(200)
+      .set(headers);
 
     const blogsAtEnd = await helper.blogsInDb();
     const updatedBlogAtEnd = blogsAtEnd.find(
