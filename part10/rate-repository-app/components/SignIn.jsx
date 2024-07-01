de5e9854ab7792cff675f419cfcbe3c0ab995fc1
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2024-07-01 12:49:18 +0200

    ✅ 10.9

--- part10/rate-repository-app/components/SignIn.jsx
+++ part10/rate-repository-app/components/SignIn.jsx
@@ -1,20 +1,36 @@
-import { SafeAreaView, TextInput, StyleSheet, Button } from "react-native";
+import {
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  Button,
+  Text,
+  View,
+} from "react-native";
 import theme from "../theme";
 import { useFormik } from "formik";
+import * as yup from "yup";
 
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+    gap: 12,
     backgroundColor: "white",
   },
   input: {
     height: 40,
-    marginBottom: 12,
     borderWidth: 1,
     padding: 10,
     borderRadius: 5,
     borderColor: theme.colors.textSecondary,
   },
+  inputError: {
+    borderColor: theme.colors.error,
+  },
+});
+
+const validationSchema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  password: yup.string().required("Password is required"),
 });
 
 const SignIn = () => {
@@ -27,24 +43,41 @@ const SignIn = () => {
       username: "",
       password: "",
     },
+    validationSchema,
     onSubmit,
   });
 
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={formik.values.username}
-        onChangeText={formik.handleChange("username")}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={formik.values.password}
-        onChangeText={formik.handleChange("password")}
-      />
+      <View>
+        <TextInput
+          style={[styles.input, formik.errors.username && styles.inputError]}
+          placeholder="Username"
+          value={formik.values.username}
+          onChangeText={formik.handleChange("username")}
+        />
+        {formik.errors.username && (
+          <Text style={{ color: theme.colors.error }}>
+            {formik.errors.username}
+          </Text>
+        )}
+      </View>
+
+      <View>
+        <TextInput
+          style={[styles.input, formik.errors.password && styles.inputError]}
+          placeholder="Password"
+          secureTextEntry
+          value={formik.values.password}
+          onChangeText={formik.handleChange("password")}
+        />
+        {formik.errors.password && (
+          <Text style={{ color: theme.colors.error }}>
+            {formik.errors.password}
+          </Text>
+        )}
+      </View>
+
       <Button title="Sign in" onPress={formik.handleSubmit} />
     </SafeAreaView>
   );
