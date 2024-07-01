a0e9dc2465abb58f05db52bc9962b86f4a43dc5d
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2024-07-01 12:38:52 +0200

    ✅ 10.8

--- part10/rate-repository-app/components/SignIn.jsx
+++ part10/rate-repository-app/components/SignIn.jsx
@@ -1,7 +1,53 @@
-import { Text } from "react-native";
+import { SafeAreaView, TextInput, StyleSheet, Button } from "react-native";
+import theme from "../theme";
+import { useFormik } from "formik";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: "white",
+  },
+  input: {
+    height: 40,
+    marginBottom: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    borderColor: theme.colors.textSecondary,
+  },
+});
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit,
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={formik.values.username}
+        onChangeText={formik.handleChange("username")}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={formik.values.password}
+        onChangeText={formik.handleChange("password")}
+      />
+      <Button title="Sign in" onPress={formik.handleSubmit} />
+    </SafeAreaView>
+  );
 };
 
 export default SignIn;
