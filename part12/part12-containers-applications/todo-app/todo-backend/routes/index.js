8860a501ef5a450b99fb0e3cce52cd5473977b2f
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2024-07-09 17:01:45 +0200

    ✅ 12.12

--- part12/part12-containers-applications/todo-app/todo-backend/routes/index.js
+++ part12/part12-containers-applications/todo-app/todo-backend/routes/index.js
@@ -1,18 +1,26 @@
-const express = require('express');
+const express = require("express");
+const { getAsync } = require("../redis");
+
 const router = express.Router();
 
-const configs = require('../util/config')
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
   });
+});
+
+router.get("/statistics", async (_, res) => {
+  const added_todos = +(await getAsync("added_todos")) || 0;
+  console.log(added_todos);
+  res.send({ added_todos });
 });
 
 module.exports = router;
--- part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { Todo } = require("../mongo");
+const { getAsync, setAsync } = require("../redis");
+
 const router = express.Router();
 
 /* GET todos listing. */
@@ -14,6 +16,11 @@ router.post("/", async (req, res) => {
     text: req.body.text,
     done: false,
   });
+
+  const todos = +(await getAsync("added_todos")) || 0;
+  console.log("todos", todos);
+  await setAsync("added_todos", todos + 1);
+
   res.send(todo);
 });
 
